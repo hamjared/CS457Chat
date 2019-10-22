8b43aec07da010b830027801bbe892d63fd7b1a2
jared <hamjared5@gmail.com>
2019-10-21 20:59:06 -0600

    asdf

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Change Directory to CS457 and then:
 ```
 
 ## Running Instructions:
+
+## Thanks to
+I referenced the following websites while developing this code:
+  - https://www.geeksforgeeks.org/socket-programming-cc/
+  - https://beej.us/guide/bgnet/
+  - http://man7.org/linux/man-pages/man2/socket.2.html
+  - https://linux.die.net/man/2/setsockopt
--- chat.cpp
+++ chat.cpp
@@ -2,6 +2,14 @@
 #include <string>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "chat.h"
 
 using namespace std;
@@ -41,7 +49,45 @@ void parseArguments(int argc, char *argv[]){
 }
 
 void startServer(int portNumber){
-    cout << "server starting on port 6324" << "\n";
+    struct sockaddr_in serverIPAddr;
+    int opt = 1;
+
+    int serverSocket_fd = socket(AF_INET, SOCK_STREAM, 0 );
+
+    if (serverSocket_fd == -1){
+        //socket creation failed, abort program
+        cerr << "Server socket creation failed. Exiting" << "\n";
+        exit(0);
+    }
+    if (setsockopt(serverSocket_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) == -1){
+        //optional step that aids in avoiding address already in use errors
+        cerr << "Server socket creation failed. Exiting" << "\n";
+        exit(0);
+    }
+
+    serverIPAddr.sin_family = AF_INET; // IPV4
+    serverIPAddr.sin_port =htons(portNumber);
+    serverIPAddr.sin_addr.s_addr = INADDR_ANY; //listen on all interfaces
+    if (bind(serverSocket_fd, (struct sockaddr*)&serverIPAddr, sizeof serverIPAddr) == -1){
+        cerr << "Server socket bind failed. Exiting" << "\n";
+        exit(0);
+    }
+
+    if(listen(serverSocket_fd, 2) == -1){
+        //set server socket to the listening state with a max backlog of 2
+        cerr << "Server socket set as listen failed. Exiting" << "\n";
+        exit(0);
+    }
+
+    size_t hostNameLength = 24;
+    char hostname[hostNameLength] ;
+    gethostname(hostname, hostNameLength);
+
+    struct hostent *hp = gethostbyname(hostname);
+    cout << "Waiting for connection on " << inet_ntoa( *( struct in_addr*)( hp -> h_addr_list[0])) << " port 6324" << "\n";
+
+
+
 }
 
 void startClient(int port, int ip){
