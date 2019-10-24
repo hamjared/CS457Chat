59d9351ae13194bd113487e7fc97d74905ee3e68
jared <hamjared5@gmail.com>
2019-10-23 20:01:49 -0600

    asdf

--- chat.cpp
+++ chat.cpp
@@ -12,6 +12,8 @@
 #include <arpa/inet.h>
 #include "chat.h"
 
+int VERSION = 457;
+
 using namespace std;
 
 // main function -
@@ -44,7 +46,7 @@ void parseArguments(int argc, char *argv[]){
             default:
                 printHelp();
         }
-    int ipAddress = parseIP(ip);
+    startClient(port,ip);
 
 }
 
@@ -97,10 +99,28 @@ void startServer(int portNumber) {
     cout << "Found a friend, you will receive first" << "\n";
     int valread;
     char buffer[1024];
+    string message;
     while (true){
         //send and receive messages
+
+
+        message.clear();
+        cout << "Friend: ";
         valread = read( newSocket , buffer, 1024);
-        cout << "Friend: " << buffer << "\n";
+        cout << decode(buffer) << "\n";
+
+        while (true) {
+            cout << "You: " ;
+            message.clear();
+            getline(cin, message);
+            if (message.length() > 140) {
+                cout << "Error: input too long\n";
+                continue;
+            }
+            break;
+        }
+
+        sendall(newSocket, encode(message).c_str(), encode(message).length());
     }
 
 
@@ -108,15 +128,51 @@ void startServer(int portNumber) {
 
 }
 
-void startClient(int port, int ip){
-    cout << "client starting" << "\n";
+void startClient(int port, string ip){
+    string message;
+    char buffer[1024];
+    int valread;
+    struct sockaddr_in server_addr;
+    int clientSocket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket_fd ==-1){
+        cerr << "client socket failed to create, exiting" << "\n";
+        exit(0);
+    }
 
+    server_addr.sin_family = AF_INET; //IPV4
+    server_addr.sin_port = htons(port);
+    if(inet_pton(AF_INET, ip.c_str(), &server_addr.sin_addr)<=0)
+    {
+        cerr << "invalid ip address, exiting" << "\n";
+        exit(0);
+    }
+    cout << "connecting to server..." ;
+    if (connect(clientSocket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
+        cerr << "connection to server failed, exiting" << "\n";
+        exit(0);
+    }
+    cout << "connected!\n";
+    cout << "connected to a friend! You send first.\n";
 
+    while(true) {
+        cout << "You: ";
+        while (true) {
+            message.clear();
+            getline(cin, message);
+            if (message.length() > 140) {
+                cout << "Error: input too long\n";
+                cout << "You: ";
+                continue;
+            }
+            break;
+        }
+
+        sendall(clientSocket_fd, encode(message).c_str() , encode(message).length());
+        valread = read(clientSocket_fd, buffer, 1024);
+        cout << "Friend: " << decode(buffer) << "\n";
+    }
 }
 
-int parseIP(string ip){
-    return 0;
-}
 
 void printHelp(){
     cout << "Help:" << "\n";
@@ -124,3 +180,82 @@ void printHelp(){
     cout << "To run the server: " << "chat" << "\n";
     cout << "To run client: " << "chat -p [port number] -s [IP address]" << "\n";
 }
+
+int sendall(int s, const char *buf, int len)  //Function from beej guide to network programming: http://beej.us/guide/bgnet/html/multi/advanced.html#sendall
+{
+    int total = 0;        // how many bytes we've sent
+    int bytesleft = len; // how many we have left to send
+    int n;
+
+    while(total < len) {
+        n = send(s, buf+total, bytesleft, 0);
+        if (n == -1) { break; }
+        total += n;
+        bytesleft -= n;
+    }
+
+
+
+    return n==-1?-1:0; // return -1 on failure, 0 on success
+}
+
+string encode(string message){
+    string encodedString;
+    char buff[4];
+    packi16(buff, VERSION);
+    buff[2] = '\0';
+    encodedString += buff[0];
+    encodedString += buff[1];
+    packi16(buff, message.length());
+    buff[2] = '\0';
+    encodedString += buff[0];
+    encodedString += buff[1];
+    encodedString += message;
+
+    return encodedString;
+}
+
+string decode(char * message){
+
+
+    char buf[4];
+    buf[0] = message[0];
+    buf[1] = message[1];
+    buf[2] = '\0';
+    int version = unpacki16(buf);
+
+    buf[0] = message[2];
+    buf[1] = message[3];
+    buf[2] = '\0';
+    int length = unpacki16(buf);
+
+
+    string decodedString;
+    for(int i = 0; i < length; i++){
+        decodedString += message[i+4];
+    }
+
+
+
+
+    return decodedString;
+
+
+
+}
+
+void packi16( char *buf, unsigned int i) // from https://beej.us/guide/bgnet/html/multi/advanced.html#serialization
+{
+    *buf++ = i>>8;
+    *buf++ = i;
+}
+
+int unpacki16( char *buf)
+{
+    int leftSide = ((int)buf[0]) << 8;
+    int rightSide = ((int)buf[1]) & 0x000000FF;
+    return leftSide | rightSide;
+}
+
+
+
--- chat.h
+++ chat.h
@@ -6,8 +6,12 @@
 void startServer(int portNumber);
 void parseArguments(int argc, char *argv[]);
 void printHelp();
-void startClient(int port, int ip);
-int parseIP(std::string ip);
+void startClient(int port, std::string);
+int sendall(int s, const char *buf, int len);
+std::string encode(std::string message);
+std::string decode(char* message);
+void packi16( char *buf, unsigned int i);
+int unpacki16( char *buf);
 
 
 #endif
